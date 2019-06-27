59a7e9fdcfb2cd74f1f463e8136add52cc98ef9a
Denis Lavrov <d.lavrov@corp.mail.ru>
2019-06-27 21:19:41 +0300

    testColor: init commit

---
+++ getDomainColor.js
@@ -0,0 +1,55 @@
+'use strict';
+window.onload = function () {
+
+function getColor(imgSrc) {
+    const stepSize =  5;
+    const defaultColor = { r: 0, g: 0, b: 0};
+    const canvas = document.querySelector('#testCanvas');
+    const context = canvas.getContext('2d');
+    const sticky = new Image();
+    let data, width, height;
+    let i = -4;
+    let dataLength;
+    const rgb = {r: 0, g: 0, b: 0};
+    let count = 0;
+
+    if(!context) {
+        return defaultColor;
+    }
+
+    // height = canvas.height =  sticky.naturalHeight || sticky.offsetHeight || sticky.height;
+    // width = canvas.width = sticky.naturalWidth || sticky.offsetWidth  || sticky.width;
+
+    sticky.src = imgSrc;
+    sticky.onload = () => {
+        //context.drawImage(sticky, 0, 0, sticky.width, sticky.height);
+        context.drawImage(sticky, 0, 0, 300, 150);
+
+        try {
+            data = context.getImageData(0, 0, 300, 150);
+        } catch (e) {
+            return defaultColor;
+        }
+
+        dataLength = data.data.length;
+        console.log(dataLength);
+        
+        while ((i += stepSize * 4) < dataLength) {
+            ++count;
+            rgb.r += data.data[i];
+            rgb.g += data.data[i + 1];
+            rgb.b += data.data[i + 2];
+        }
+
+        rgb.r = parseInt((rgb.r/count));
+        rgb.g = parseInt((rgb.g/count));
+        rgb.b = parseInt((rgb.b/count));
+
+
+        console.log(rgb);
+        return rgb;
+    }
+}
+
+getColor('img/img2.jpg');
+};
