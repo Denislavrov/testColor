3a7aff73d80f451247454c33be76d460180ac9b5
Denis Lavrov <d.lavrov@corp.mail.ru>
2019-07-09 15:03:44 +0300

    testColor: fix bag -rgb and function domainColor

--- getDomainColor.js
+++ getDomainColor.js
@@ -1,119 +1,134 @@
 'use strict';
 window.onload = function () {
 
-function getColor(imgSrc) {
-    const stepSize =  5;
-    const defaultColor = { r: 0, g: 0, b: 0};
-    const canvas = document.querySelector('#testCanvas');
-    const context = canvas.getContext('2d');
-    const sticky = new Image();
-    let data, width, height;
-    let i = -4;
-    let dataLength;
-    const rgb = {r: 0, g: 0, b: 0};
-    let count = 0;
-    let backgroundColorStyle = document.querySelector('.colorMarker');
+    const settings = {
+        img: 'img/default/img3.jpg',
+        backgroundOpacity: 0.7,
+        colorsButtonKoef: 20,
+        darkDefaultBGColor: '#9e9e9e',
+        darkText: '#333',
+        lightText: '#fff',
+        canvasId: '#testCanvas',
+        backgroundColorClass: 'colorMarker',
+        buttonClass: 'canvas__button',
+        domainClass: 'canvas__domain',
+    };
 
-    if(!context) {
-        return defaultColor;
-    }
-    sticky.src = imgSrc;
-    sticky.onload = () => {
-         //height = canvas.height =  sticky.naturalHeight || sticky.offsetHeight || sticky.height;
-         //width = canvas.width = sticky.naturalWidth || sticky.offsetWidth  || sticky.width;
+    function getColor(imgSrc) {
+        const stepSize = 5;
+        const defaultColor = {r: 0, g: 0, b: 0};
+        const canvas = document.querySelector(settings.canvasId);
+        const context = canvas.getContext('2d');
+        const sticky = new Image();
+        let data, width, height;
+        let i = -4;
+        let dataLength;
+        const rgb = {r: 0, g: 0, b: 0};
+        let count = 0;
+        let backgroundColorStyle = document.querySelector(`.${settings.backgroundColorClass}`);
 
-        context.drawImage(sticky, 0, 0, 298, 168);
-
-        try {
-            data = context.getImageData(0, 0, 298, 168);
-        } catch (e) {
+        if (!context) {
             return defaultColor;
         }
+        sticky.src = imgSrc;
+        sticky.setAttribute('crossOrigin', '');
+        sticky.onload = () => {
+            //height = canvas.height =  sticky.naturalHeight || sticky.offsetHeight || sticky.height;
+            //width = canvas.width = sticky.naturalWidth || sticky.offsetWidth  || sticky.width;
 
-        dataLength = data.data.length;
-        
-        while ((i += stepSize * 4) < dataLength) {
-            ++count;
-            rgb.r += data.data[i];
-            rgb.g += data.data[i + 1];
-            rgb.b += data.data[i + 2];
-        }
+            context.drawImage(sticky, 0, 0, 298, 168);
 
-        rgb.r = parseInt((rgb.r/count));
-        rgb.g = parseInt((rgb.g/count));
-        rgb.b = parseInt((rgb.b/count));
+            try {
+                data = context.getImageData(0, 0, 298, 168);
+            } catch (e) {
+                return defaultColor;
+            }
 
+            dataLength = data.data.length;
 
-       getTextColor(backgroundColorStyle.style.backgroundColor = `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`, 'colorMarker');
-       getButtonBackground(backgroundColorStyle.style.backgroundColor = `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`);
-        return backgroundColorStyle.style.backgroundColor = `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.7)`;
-    };
-}
+            while ((i += stepSize * 4) < dataLength) {
+                ++count;
+                rgb.r += data.data[i];
+                rgb.g += data.data[i + 1];
+                rgb.b += data.data[i + 2];
+            }
 
-// function getTextColor(backgroundColor) {
-//     const colorMarker =  document.querySelector('.colorMarker');
-//     let rgb = colorMarker.style.backgroundColor = backgroundColor;
-//     let colors = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-//     let brightness = 3;
-//
-//     console.log(colors, 'colors');
-//     let r = colors[1];
-//     let g = colors[2];
-//     let b = colors[3];
-//
-//     let ir = Math.floor((255 - r) * brightness);
-//     let ig = Math.floor((255 - g) * brightness);
-//     let ib = Math.floor((255 - b) * brightness);
-//
-//     return colorMarker.style.color = `rgb(${ir}, ${ig}, ${ib})`;
-// }
+            rgb.r = parseInt((rgb.r / count));
+            rgb.g = parseInt((rgb.g / count));
+            rgb.b = parseInt((rgb.b / count));
 
-function getTextColor(backgroundColor, textClassName) {
-
-    debugger;
-   const colorMarker = document.querySelector(`.${textClassName}`);
-   let rgb = colorMarker.style.backgroundColor = backgroundColor;
-   let colors = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-   let r = colors[1];
-   let g = colors[2];
-   let b = colors[3];
-
-
-
-   let hexColor = getHexColor(r,g,b);
-   let hexColorInt = parseInt(hexColor.substr(1,6),16);
-   let firstNum = parseInt(hexColor.substr(1,1), 16);
-    /* цвет #A9A9A9	rgb(169, 169, 169) в 10- чной системе счисления для определения цвета текста*/
-   const blackLimiter = 11119017;
-    if(blackLimiter < hexColorInt) {
-        return colorMarker.style.color = `#000`;
-    } else {
-        return colorMarker.style.color = `#fff`;
+            getTextColor(backgroundColorStyle.style.backgroundColor = `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`, settings.backgroundColorClass);
+            getButtonBackground(backgroundColorStyle.style.backgroundColor = `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`);
+            getDomainColor(backgroundColorStyle.style.backgroundColor = `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`);
+            return backgroundColorStyle.style.backgroundColor = `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, ${settings.backgroundOpacity})`;
+        };
     }
-}
 
-function componentToHex(color) {
+    function getTextColor(backgroundColor, textClassName) {
+        const colorMarker = document.querySelector(`.${textClassName}`);
+        let rgb = colorMarker.style.backgroundColor = backgroundColor;
+        let colors = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+        let r = colors[1];
+        let g = colors[2];
+        let b = colors[3];
 
-    let hex = parseInt(color).toString(16);
-    return hex.length === 1 ? "0" + hex : hex;
-}
-function getHexColor(r,g,b) {
-    return '#'+ componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
 
-function getButtonBackground(backGroundColor) {
-    debugger;
-const colorMarker = document.querySelector('.canvas__button');
-const colors = backGroundColor.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-const colorsKoef = 20;
-console.log(colors);
-let r = parseInt(colors[1], 10) - colorsKoef;
-let g = parseInt(colors[2], 10) - colorsKoef;
-let b = parseInt(colors[3], 10) - colorsKoef;
-console.log(r,g,b);
-getTextColor(colorMarker.style.backgroundColor = `rgb(${r}, ${g}, ${b})`, 'canvas__button');
-return colorMarker.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-}
+        let hexColor = getHexColor(r, g, b);
+        let hexColorInt = parseInt(hexColor.substr(1, 6), 16);
+        let firstNum = parseInt(hexColor.substr(1, 1), 16);
+        /* цвет #A9A9A9	rgb(169, 169, 169) в 10- чной системе счисления для определения цвета текста*/
+        const blackLimiter = 11119017;
+        if (blackLimiter < hexColorInt) {
+            return colorMarker.style.color = settings.darkText;
+        } else {
+            return colorMarker.style.color = settings.lightText;
+        }
+    }
 
-getColor('img/img5.png');
+    function getButtonBackground(backGroundColor) {
+        const colorMarker = document.querySelector(`.${settings.buttonClass}`);
+        const colors = backGroundColor.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+        const colorsKoef = settings.colorsButtonKoef;
+
+        let r = parseInt(colors[1], 10);
+        let g = parseInt(colors[2], 10);
+        let b = parseInt(colors[3], 10);
+
+        if (r > 50 && b > 50 && b > 50) {
+            getTextColor(colorMarker.style.backgroundColor = `rgb(${r - colorsKoef}, ${g - colorsKoef}, ${b - colorsKoef})`, settings.buttonClass);
+            return colorMarker.style.backgroundColor = `rgb(${r - colorsKoef}, ${g - colorsKoef}, ${b - colorsKoef})`;
+        } else {
+            colorMarker.style.color = settings.lightText;
+            return colorMarker.style.backgroundColor = settings.darkDefaultBGColor;
+
+        }
+    }
+
+    function getDomainColor(backGroundColor) {
+        const colorMarker = document.querySelector(`.${settings.domainClass}`);
+        const colors = backGroundColor.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+        const colorsKoef = settings.colorsButtonKoef;
+
+        let r = parseInt(colors[1], 10);
+        let g = parseInt(colors[2], 10);
+        let b = parseInt(colors[3], 10);
+
+        if (r > 50 && b > 50 && b > 50) {
+            return colorMarker.style.color = `rgb(${r - colorsKoef}, ${g - colorsKoef}, ${b - colorsKoef})`;
+        } else {
+            return colorMarker.style.color = settings.darkDefaultBGColor;
+        }
+    }
+
+    function componentToHex(color) {
+        let hex = parseInt(color).toString(16);
+        return hex.length === 1 ? "0" + hex : hex;
+    }
+
+    function getHexColor(r, g, b) {
+        return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    }
+
+    getColor(settings.img);
 };
+
